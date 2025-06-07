59c7d2a14fc92232a44a6f4fd516a838a3320b56
jvictorportela <jvictorportela30@outlook.com>
2025-06-07 08:40:36 -0300

    v1

--- src/Backend/Zeal.Application/obj/Debug/net8.0/Zeal.Application.AssemblyInfo.cs
+++ src/Backend/Zeal.Application/obj/Debug/net8.0/Zeal.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zeal.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b84e207d97e3cda4f651f5101dd9d2bb1c3485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3fe5765de98c88d99798dc243e1e6eee8fd697f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zeal.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zeal.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
